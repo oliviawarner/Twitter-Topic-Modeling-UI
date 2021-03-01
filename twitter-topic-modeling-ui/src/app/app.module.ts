af72b36eb21b403dc5f18ed3d64cdf39e93a7a77
Olivia Warner <ojw1002@sru.edu>
2021-03-01 15:52:52 -0500

    added home component

--- twitter-topic-modeling-ui/src/app/app.module.ts
+++ twitter-topic-modeling-ui/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {LoginComponent} from './login/login.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
--- twitter-topic-modeling-ui/src/app/login/login.component.ts
+++ twitter-topic-modeling-ui/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component ({
     selector: 'app-login',
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
 })
 
 export class LoginComponent {
-  
+
+    
 }
