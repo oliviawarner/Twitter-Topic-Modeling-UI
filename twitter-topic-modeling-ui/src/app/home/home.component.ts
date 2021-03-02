69b8675c93790e05d9c1b41b080d27350795ee73
oliviawarner <ojw1002@sru.edu>
2021-03-02 16:25:42 -0500

    home component edit

--- twitter-topic-modeling-ui/src/app/home/home.component.ts
+++ twitter-topic-modeling-ui/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component ({
@@ -8,4 +9,6 @@ import { Component } from '@angular/core';
 })
 
 export class HomeComponent {
+    //router constructor
+  constructor(private router: Router) { }
 }
