bcf47155cceaa4565aaebc722c1341fa2e67db0e
oliviawarner <ojw1002@sru.edu>
2021-03-02 13:02:16 -0500

    onclick show home

--- twitter-topic-modeling-ui/src/app/app.module.ts
+++ twitter-topic-modeling-ui/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
@@ -12,7 +12,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- twitter-topic-modeling-ui/src/app/login/login.component.ts
+++ twitter-topic-modeling-ui/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component ({
@@ -7,7 +7,19 @@ import { Router } from '@angular/router';
     styleUrls: ['./login.component.css']
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit  {
 
-    
+        //router constructor
+        constructor(private router: Router) { }
+        
+        username: string;
+        password: string;
+
+        ngOnInit() {
+        }
+
+        goHome() : void {
+            this.router.navigate(["home-component"]);
+        }
+         
 }
